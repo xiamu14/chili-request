cc9e5f69c9749ca69990eef2b08023934d0bbe52
ben14 <544652471@qq.com>
2019-12-09 10:16:48 +0800

    feat: adjust usage.

--- lib/hooks/use_chilireq.d.ts
+++ lib/hooks/use_chilireq.d.ts
@@ -1,4 +1,4 @@
 import React from 'react';
-import { ReqConfig, BaseConfig } from '../type';
+import { BaseConfig } from '../type';
 export declare const ChiliReqContext: React.Context<BaseConfig | undefined>;
-export declare function useChiliReq<T>(regConfig: ReqConfig): () => Promise<T>;
+export declare function useChiliReq(): <T>(regConfig: import("../type").ReqConfig) => Promise<T>;
--- src/hooks/use_chilireq.ts
+++ src/hooks/use_chilireq.ts
@@ -1,21 +1,15 @@
 import React, { useContext } from 'react';
 import chiliReqBase from '../chili_req_base';
-import { ReqConfig, BaseConfig } from '../type';
+import { BaseConfig } from '../type';
 
 export const ChiliReqContext = React.createContext<BaseConfig | undefined>(
   undefined,
 );
 
-export function useChiliReq<T>(regConfig: ReqConfig) {
+export function useChiliReq() {
   const config = useContext(ChiliReqContext);
   if (config === undefined) {
     throw 'chiliReq missing baseConfig.';
   }
-  return () => {
-     return new Promise<T>((resolve, reject) => {
-      chiliReqBase(config)<T>(regConfig)
-        .then(res => resolve(res))
-        .catch(error => reject(error));
-    });
-  };
+  return chiliReqBase(config);
 }
