6df57343c405b785fdcc7de2229769511b506c87
ben <544652471@qq.com>
2020-02-21 09:14:55 +0800

    feat: build and publish.

--- lib/helper/chilireq_context.d.ts
+++ lib/helper/chilireq_context.d.ts
@@ -1,4 +1,3 @@
 import React from 'react';
 import { BaseConfig } from '../type';
 export declare const ChiliReqContext: React.Context<BaseConfig | undefined>;
-export declare function useChiliReq(): () => <T>(regConfig: import("../type").ReqConfig) => Promise<T>;
